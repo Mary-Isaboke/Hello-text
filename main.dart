4a104524dbdd2c81921b3a7104c1be814264e92c
mary issa <merianmary468@gmail.com>
2024-03-11 21:34:03 +0300

    new erra

---
+++ main.dart
@@ -0,0 +1,13 @@
+void main() {
+  print("Hello world");
+  print("This is it");
+  print("My first program");
+  print("Lets do this together");
+}
+  
+
+
+
+  
+
+
---
+++ variables.dart
@@ -0,0 +1,17 @@
+void main(){
+  // variables
+ // String name ="Mary";
+ 
+  int age = 20;
+  String nationality = "Kenyan";
+  bool ismarried = false;
+//print output
+ // print(name);
+  print(age);
+  print(nationality);
+  print(ismarried);
+  print("Hello there my name is mary and am $age year old, my nationality is $nationality and my marriage status is married");
+}
+
+
+
